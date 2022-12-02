f96d691e411c252bcf473ff65443450e3018d1ae
Polina Guseva (U-course) <pn_guseva@mail.ru>
2022-12-02 15:01:06 +0300

    task-react-render-and-effects

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,5 +1,21 @@
+import { useState, useEffect } from 'react';
 import { subscribe, unsubscribe } from './resources/API';
 
 export function Effects(props: { sourceId: string }) {
-    return <div>123</div>;
+    const [lastMessage, setlastMessage] = useState(-1);
+    const setMessage = (message: number) => {
+        setlastMessage(message);
+    };
+    useEffect(() => {
+        subscribe(props.sourceId, setMessage);
+        return () => {
+            setlastMessage(-1);
+            unsubscribe(props.sourceId, setMessage);
+        };
+    }, [props.sourceId]);
+    return (
+        <div>
+            {props.sourceId}: {lastMessage}
+        </div>
+    );
 }
